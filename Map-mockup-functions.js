2d909efa45593b1bb4a1ec28178ee0d73ccbe238
Zakhya <42898990+Zakhya@users.noreply.github.com>
2023-08-10 23:51:03 -0400

    added 2 races to map mockup section

--- Map-mockup-functions.js
+++ Map-mockup-functions.js
@@ -1,4 +1,18 @@
-// Header color change on mouseOver
+let gridLength = 396
+let mapTile = []
+
+for (let i = 0; i < gridLength ; i++) {
+    mapTile[i] = document.createElement('span')
+    mapTile[i].setAttribute('class', 'button number')
+    index = i.toString()
+    document.getElementById('buttons').appendChild(mapTile[i])
+    mapTile[i].race = 'none'
+    mapTile[i].id = i
+    mapTile[i].rowNumber = Math.floor(i / 22) + 1
+    mapTile[i].columnNumber = i % 22 + 1
+    mapTile[i].ip = 0  
+}
+
 let flex1 = document.getElementById('flexbox-item1')
 let flex2 = document.getElementById('flexbox-item2')
 let flex3 = document.getElementById('flexbox-item3')
@@ -25,25 +39,6 @@ document.getElementById('flexbox-item3').addEventListener('pointerleave', functi
     flex3.style.background = '#006600';
     })
 
-
-// let dropDownValue = 'ent'
-// document.getElementById('building-selector').addEventListener('change', function (e) {
-//     dropDownValue = e.target.value
-// })
-let colors = ["blue", "yellow", "orange"];
-let currentColor = 0;
-let gridLength = 396
-let mapTile = []
-
-for (let i = 0; i < gridLength ; i++) {
-    mapTile[i] = document.createElement('span')
-    mapTile[i].setAttribute('class', 'button number')
-    index = i.toString()
-    mapTile[i].setAttribute('id', index)
-    document.getElementById('buttons').appendChild(mapTile[i])
-    
-    
-}
 for (let i = 0; i < mapTile.length; i++) {
     mapTile[i].tileValue = 0
     mapTile[i].addEventListener('pointerenter', function  (e) {
@@ -55,51 +50,77 @@ for (let i = 0; i < mapTile.length; i++) {
         if (mapTile[i].tileValue === 1) {
         this.style.background = 'white';
         mapTile[i].tileValue = 0}})
-    
-    mapTile[i].addEventListener('click', function (e) {
-           //change color based on dropdown menu
-           let dropDownValue = document.getElementById('building-selector').value
-          if (dropDownValue === 'ent') {
-            this.style.background = 'green'
+        mapTile[i].addEventListener('click', function (e) {
+            //change color based on dropdown menu
+            let dropDownValue = document.getElementById('building-selector').value
+           if (dropDownValue === 'ent') {
+             this.style.background = 'green'
+             console.log(`column: ${this.columnNumber}`)
+             console.log(this.rowNumber)
+             this.race = 'ent'
+             
+             
+             this.innerText = `Race:${this.race} \n IP:${this.ip}`
              mapTile[i].tileValue = 2
-          } else {
+             ipCalc()
+        } else if (dropDownValue === 'human') {
+            this.style.background = 'yellow'
+            console.log(`column: ${this.columnNumber}`)
+            console.log(this.rowNumber)
+            this.race = 'hmn'
             
-          }
-        //    this.style.background = 'blue'
+            
+            this.innerText = `Race:${this.race} \n IP:${this.ip}`
+            mapTile[i].tileValue = 2
+            ipCalc()
+       }
     })
+}    
+
+    function ipCalc() {
+        for (let i = 0; i < mapTile.length; i++) {
+          let ip = 0;
+          let entsAround = 0
+          let humansAround = 0
+            // Calculate the range of neighboring cells (3x3 grid)
+            for (let rowOffset = -2; rowOffset <= 2; rowOffset++) {
+              for (let colOffset = -2; colOffset <= 2; colOffset++) {
+                const neighborRow = mapTile[i].rowNumber + rowOffset;
+                const neighborCol = mapTile[i].columnNumber + colOffset;
+                // Check if the neighbor is within bounds
+                if (
+                  neighborRow >= 1 &&
+                  neighborRow <= Math.ceil(gridLength / 22) &&
+                  neighborCol >= 1 &&
+                  neighborCol <= 22
+                ) {
+                  const neighborIndex = (neighborRow - 1) * 22 + neighborCol - 1;
+                  // Check if the neighbor is an 'ent'
+                  if (mapTile[neighborIndex].race === 'ent') {
+                    entsAround++;
+                } else if(mapTile[neighborIndex].race === 'hmn'){
+                      humansAround++;
+                  }
+                }
+              }
+            }
+            if(mapTile[i].race === 'ent'){
+                ip += entsAround;
+                ip -= humansAround
+                
+            } else if (mapTile[i].race === 'hmn'){
+                ip -= entsAround;
+                ip += humansAround
+
+            }
+          
+
+        mapTile[i].ip = ip;
+        if(mapTile[i].race !== 'none'){
+            mapTile[i].innerText = `Race: ${mapTile[i].race}\nIP: ${mapTile[i].ip}`;
+        }
+    }
 }
 
 
-
-
-// var colors = ["blue", "yellow", "orange"];
-// var gridLength = 396;
-
-// // Create the grid items and store them in the mapTile array
-// let tileArray = []
-// class MapTile  {
-//     constructor (currentColor, colors) {
-//     this.currentColor = 0
-//     this.colors = ["white", "blue", "yellow", "orange"]
-// }};
-// for (var i = 0; i < gridLength; i++) {
-//     let mapTile = new MapTile(0, 'white')
-//     mapTile[i] = document.createElement("div");
-//     mapTile[i].setAttribute('class', 'button number')
-//     index = i.toString()
-//     mapTile[i].setAttribute('id', index)
-//     document.getElementById('buttons').appendChild(mapTile[i]);
-//     tileArray.push(mapTile)
-// }
-// console.log(tileArray)
-
-// // Add the click event listener to each grid item
-// for (var i = 0; i < gridLength; i++) {
-//     mapTile[i].addEventListener("click", function() {
-//         this.style.backgroundColor = colors[0];
-//         mapTile[i].currentColor++;
-//         if (currentColor >= colors.length) {
-//             currentColor = 0;
-//         }
-//     });
-// }
+console.log(mapTile)
